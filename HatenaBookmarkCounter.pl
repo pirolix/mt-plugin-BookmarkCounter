a6668d74614f8a2252ffc0d4f7f2018195379eb2
piroli <piroli@adbff654-c4ea-448a-8ae3-cad037306009>
2010-01-22 23:24:34 +0000

    0.01
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/BookmarkCounter/trunk@110 adbff654-c4ea-448a-8ae3-cad037306009

---
+++ HatenaBookmarkCounter.pl
@@ -0,0 +1,28 @@
+package MT::Plugin::SKR::HatenaBookmarkCounter;
+# @see 
+
+use strict;
+use MT 4;
+use base qw( MT::Plugin );
+my $plugin = __PACKAGE__->new({
+    id => __PACKAGE__,
+    key => __PACKAGE__,
+    name => __PACKAGE__,
+});
+
+### for MT4/MT5
+MT->add_callback ('MT::App::CMS::template_source.entry_table', 5, $plugin, sub {
+    my ($eh, $app, $tmpl) = @_;
+
+    my $old = quotemeta (<<'HTMLHEREDOC');
+<a href="<$mt:var name="entry_permalink"$>" target="<__trans phrase="_external_link_target">" title="<mt:if name="object_type" eq="entry"><__trans phrase="View entry"><mt:else><__trans phrase="View page"></mt:if>"><img src="<$mt:var name="static_uri"$>images/spacer.gif" alt="<mt:if name="object_type" eq="entry"><__trans phrase="View entry"><mt:else><__trans phrase="View page"></mt:if>" width="13" height="9" /></a>
+HTMLHEREDOC
+    my $new = <<'HTMLHEREDOC';
+<br /><a href="http://b.hatena.ne.jp/entry/<$mt:var name="entry_permalink"$>"><img src="http://b.hatena.ne.jp/entry/image/<$mt:var name="entry_permalink"$>" /></a>
+HTMLHEREDOC
+    $$tmpl =~ s!($old)!$1$new!;
+});
+
+1;
+__END__
+
---
+++ LivedoorClipCounter.pl
@@ -0,0 +1,26 @@
+package MT::Plugin::SKR::LivedoorClipCounter;
+# @see 
+
+use strict;
+use MT 4;
+use base qw( MT::Plugin );
+my $plugin = __PACKAGE__->new({
+    id => __PACKAGE__,
+    key => __PACKAGE__,
+    name => __PACKAGE__,
+});
+
+### for MT4/MT5
+MT->add_callback ('MT::App::CMS::template_source.entry_table', 5, $plugin, sub {
+    my ($eh, $app, $tmpl) = @_;
+
+    my $old = quotemeta (<<'HTMLHEREDOC');
+<a href="<$mt:var name="entry_permalink"$>" target="<__trans phrase="_external_link_target">" title="<mt:if name="object_type" eq="entry"><__trans phrase="View entry"><mt:else><__trans phrase="View page"></mt:if>"><img src="<$mt:var name="static_uri"$>images/spacer.gif" alt="<mt:if name="object_type" eq="entry"><__trans phrase="View entry"><mt:else><__trans phrase="View page"></mt:if>" width="13" height="9" /></a>
+HTMLHEREDOC
+    my $new = <<'HTMLHEREDOC';
+<br /><a href="http://clip.livedoor.com/page/<$mt:var name="entry_permalink"$>"><img src="http://image.clip.livedoor.com/counter/<$mt:var name="entry_permalink"$>" /></a>
+HTMLHEREDOC
+    $$tmpl =~ s!($old)!$1$new!;
+});
+
+1;
